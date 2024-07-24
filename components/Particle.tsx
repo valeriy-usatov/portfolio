55f1a1bcf962cbae341fdaf3151043e6384bdfa5
Usatov <valeriy-usatov@yandex.ru>
2024-07-24 10:45:25 +0700

    first commit

---
+++ components/Particle.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import { useCallback } from 'react'
+import Particles from 'react-tsparticles'
+import { loadFull } from 'tsparticles'
+
+const Particle = () => {
+  return (
+    <div>Particle</div>
+  )
+}
+
+export default Particle
